c483765c8e54520492a41bce79db4dbac99d99bf
Lê Quang Trường <quangtruongle369@gmail.com>
2022-12-04 15:43:53 +0700

    upload

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ connectDB();
 
 let port = process.env.PORT || 8080;  //Port === undefined => Port = 6060
 
+//Port update
 app.listen(port, () => {
     //callback
     console.log("Backend Nodejs is running on the port: " + port);
